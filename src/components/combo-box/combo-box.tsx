fd6ac173e3db6d7b0b29ed05ee652ff5dd5d7f02
Vladimir Borisov <1mt-2@mail.ru>
2022-12-07 21:47:39 +0300

    feat: added autocomplete in input

--- src/components/combo-box/combo-box.tsx
+++ src/components/combo-box/combo-box.tsx
@@ -1,68 +1,123 @@
-import React, {useEffect, useState} from 'react'
-import s from './combo-box.module.css'
+import React, { useEffect, useState } from 'react';
+import s from './combo-box.module.css';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export type ListItemType = {
-  id: number,
-  name: string
-}
+  id: number;
+  name: string;
+};
 
 const itemList: Array<ListItemType> = [
   {
     id: 1,
-    name: 'option1'
+    name: 'MOSCOW'
   },
   {
     id: 2,
-    name: 'option2'
+    name: 'KALUGA'
   },
   {
     id: 3,
-    name: 'option3'
+    name: 'TULA'
   },
   {
     id: 4,
-    name: 'option4'
+    name: 'BRYANSK'
   },
   {
     id: 5,
-    name: 'option5'
+    name: 'SOCHI'
   },
   {
     id: 6,
-    name: 'option6'
+    name: 'EKATERINBURG'
   },
   {
     id: 7,
-    name: 'option7'
+    name: 'ROSTOV'
   },
   {
     id: 8,
-    name: 'option8'
+    name: 'VORONEZH'
   },
   {
     id: 9,
-    name: 'option9'
-  },
-]
-
+    name: 'MINSK'
+  }
+];
 
 export function ComboBox() {
-
-  const [list, setList] = useState<Array<ListItemType>>([])
+  const [list, setList] = useState<Array<ListItemType>>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isFolded, setIsFolded] = useState<boolean>(true);
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
-      setList(itemList)
-    }, 500)
+      setList(itemList);
+    }, 500);
     return () => {
-      clearTimeout(timeOut)
-    }
-  })
+      clearTimeout(timeOut);
+    };
+  });
 
-  return <div className={s.comboBoxContainer}>
-    <div className={s.selectLine}> Selection list</div>
-    <ul className={s.dropDownListStyle}>
-      {list.map(l => <li key={l.id}>{l.name}</li>)}
-    </ul>
-  </div>;
+  const onChangeHandler = (currentValue: string) => {
+    setInputValue(currentValue);
+  };
+
+  const onLiClickHandler = (name: string) => {
+    setInputValue(name);
+    setIsFolded(true);
+  };
+
+  return (
+    <div className={s.comboBoxContainer}>
+      <div className={s.selectLine}>
+        <AiOutlineSearch size={18} className={s.searchSvg} />
+        <input
+          onClick={() => {
+            setIsFolded(false);
+          }}
+          onChange={(e) => onChangeHandler(e.currentTarget.value)}
+          value={inputValue}
+          type="text"
+          placeholder={'Enter something'}
+        />
+        {isFolded ? (
+          <BiChevronDown
+            onClick={() => setIsFolded(!isFolded)}
+            size={20}
+            className={s.biChevronSvg}
+          />
+        ) : (
+          <BiChevronUp
+            onClick={() => setIsFolded(!isFolded)}
+            size={20}
+            className={s.biChevronSvg}
+          />
+        )}
+      </div>
+      <ul
+        className={`${
+          isFolded ? s.dropDownListFoldedStyle : s.dropDownListStyle
+        }`}
+      >
+        {list.map((l) => (
+          <li
+            key={l.id}
+            className={`${
+              l.name.toLowerCase().includes(inputValue.toLowerCase())
+                ? s.displayItems
+                : s.hideItems
+            }`}
+            onClick={() => {
+              onLiClickHandler(l.name);
+            }}
+          >
+            {l.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
